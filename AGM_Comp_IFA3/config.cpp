3321453b9d5e99d3fc5fc95d477266437e32b53a
Fadi <sdanon10110@gmail.com>
2014-12-08 21:50:43 -0600

    Pilots, missing ;s, RU MGs

--- AGM_Comp_IFA3/config.cpp
+++ AGM_Comp_IFA3/config.cpp
@@ -21,7 +21,7 @@ class cfgWeapons {
   };
 
   class LIB_MLMG42: LIB_MLMG {
-    // CSW variant (trench) so no bipod.
+    // CSW variant
     AGM_Bipod = 0;
     AGM_Overheating_allowSwapBarrel = 1;
   };
@@ -33,17 +33,35 @@ class cfgWeapons {
     AGM_Overheating_allowSwapBarrel = 0;
   };
 
+  class LIB_DP28: LIB_LMG {
+    AGM_Bipod = 1;
+    AGM_Overheating_allowSwapBarrel = 0;
+  };
+
+  class LIB_DT: LIB_LMG {
+    AGM_Bipod = 1;
+    AGM_Overheating_allowSwapBarrel = 0;
+  };
+
   class Launcher;
   class LIB_PzFaust_30m: Launcher {
     AGM_Backblast_Angle = 60;
     AGM_Backblast_Range = 15;
     AGM_Backblast_Damage = 0.7;
-  }
+  };
 };
 
 class cfgVehicles {
   class LIB_tank_base: Tank
   {
     AGM_FCSEnabled = 0;
+  };
+
+  class LIB_SOV_pilot: LIB_SOV_soldier_base {
+  	AGM_GForceCoef = 0.75; // Should this be lower? Pilots having no G-suit and such...
+  };
+
+  class LIB_GER_pilot: LIB_GER_soldier_base {
+  	AGM_GForceCoef = 0.75; // Should this be lower? Pilots having no G-suit and such...
   };
 };
