9b5e82461574beab518da3b44057c0cc22f240ff
Fadi <sdanon10110@gmail.com>
2014-12-08 21:41:51 -0600

    First commit

---
+++ AGM_Comp_IFA3/config.cpp
@@ -0,0 +1,49 @@
+class CfgPatches {
+  class AGM_Comp_IFA3 {
+    units[] = {};
+    weapons[] = {};
+    requiredVersion = 0.1;
+    requiredAddons[] = {AiA_BaseConfig_F, AiA_Weapons_IF, LIB_Weapons_A3};
+    version = "1.0";
+    versionStr = "1.0";
+    versionAr[] = {1,0,0};
+    author[] = {"Fadi"};
+  };
+};
+
+class cfgWeapons {
+
+  class LIB_LMG;
+
+  class LIB_MG42: LIB_LMG {
+    AGM_Bipod = 1;
+    AGM_Overheating_allowSwapBarrel = 1;
+  };
+
+  class LIB_MLMG42: LIB_MLMG {
+    // CSW variant (trench) so no bipod.
+    AGM_Bipod = 0;
+    AGM_Overheating_allowSwapBarrel = 1;
+  };
+
+  class LIB_MG42_Tripod: LIB_MLMG42 {};
+
+  class LIB_MG42_veh: LIB_MG42_Tripod {
+    AGM_Bipod = 0;
+    AGM_Overheating_allowSwapBarrel = 0;
+  };
+
+  class Launcher;
+  class LIB_PzFaust_30m: Launcher {
+    AGM_Backblast_Angle = 60;
+    AGM_Backblast_Range = 15;
+    AGM_Backblast_Damage = 0.7;
+  }
+};
+
+class cfgVehicles {
+  class LIB_tank_base: Tank
+  {
+    AGM_FCSEnabled = 0;
+  };
+};
