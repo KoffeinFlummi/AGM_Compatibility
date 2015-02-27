310b9c0bcda469cd5462b0dfa4158719823904b0
SAM <simon@agius-muscat.net>
2015-02-27 17:31:15 +0100

    Adjusted Stinger values

--- AGM_Comp_CUP_Weapons_Strela/config.cpp
+++ AGM_Comp_CUP_Weapons_Strela/config.cpp
@@ -3,7 +3,7 @@ class CfgPatches {
     units[] = {};
     weapons[] = {};
     requiredVersion = 0.1;
-	requiredAddons[] = {AGM_Comp_CUP_Weapons_Ammunition,CUP_Weapons_Ammunition,CUP_Weapons_Strela_2};
+	requiredAddons[] = {CUP_Weapons_Strela_2};
     version = "1.0";
     versionStr = "1.0";
     versionAr[] = {1,0,0};
@@ -15,10 +15,9 @@ class CfgPatches {
 class CfgWeapons {
   class Launcher_Base_F;
   // CUP_Weapons_Strela_2
-  // Requires adjusting to realistic values
   class CUP_launch_9K32Strela: Launcher_Base_F {
     AGM_Backblast_Angle = 45;
-    AGM_Backblast_Range = 100;
-    AGM_Backblast_Damage = 0.7;
+    AGM_Backblast_Range = 15;
+    AGM_Backblast_Damage = 0.3; // Similar to stinger, could not find a reference.
   };
 };
