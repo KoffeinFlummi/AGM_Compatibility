9c69ef6ccde2f35824bf918592644954ce7ffe3d
SAM <simon@agius-muscat.net>
2015-02-27 17:28:53 +0100

    Adjusted Stinger values
    http://www.globalsecurity.org/military/library/policy/army/fm/44-18-1/Ch2.htm

--- AGM_Comp_CUP_Weapons_Stinger/config.cpp
+++ AGM_Comp_CUP_Weapons_Stinger/config.cpp
@@ -3,7 +3,7 @@ class CfgPatches {
     units[] = {};
     weapons[] = {};
     requiredVersion = 0.1;
-    requiredAddons[] = {AGM_Comp_CUP_Weapons_Ammunition,CUP_Weapons_Ammunition,CUP_Weapons_Stinger};
+    requiredAddons[] = {CUP_Weapons_Stinger};
     version = "1.0";
     versionStr = "1.0";
     versionAr[] = {1,0,0};
@@ -15,10 +15,9 @@ class CfgPatches {
 class CfgWeapons {
   class Launcher_Base_F;
   // CUP_Weapons_Stinger
-  // Requires adjusting to realistic values
   class CUP_launch_FIM92Stinger: Launcher_Base_F {
     AGM_Backblast_Angle = 45;
-    AGM_Backblast_Range = 100;
-    AGM_Backblast_Damage = 0.7;
+    AGM_Backblast_Range = 15;
+    AGM_Backblast_Damage = 0.3; // Main damage arises from debris rather than the actual backblast
   };
 };
