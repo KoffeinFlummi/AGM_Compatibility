d7028be96208bc7b314a90903b6f29fd15c79032
SAM <simon@agius-muscat.net>
2015-02-27 16:40:49 +0100

    Removed unrequired PBOs, changed tabs to spaces

--- AGM_Comp_CUP_Weapons_Javelin/config.cpp
+++ AGM_Comp_CUP_Weapons_Javelin/config.cpp
@@ -1,15 +1,14 @@
 class CfgPatches {
   class AGM_Comp_CUP_Weapons_Javelin {
     units[] = {};
-	weapons[] = {};
-	requiredVersion = 0.1;
-	requiredAddons[] = {AGM_Comp_CUP_Weapons_Ammunition,
-	CUP_Weapons_Ammunition,CUP_Weapons_Javelin};
-	version = "1.0";
-	versionStr = "1.0";
-	versionAr[] = {1,0,0};
-	author[] = {"Winter"};
-	authorUrl = "https://github.com/Winter259";
+    weapons[] = {};
+    requiredVersion = 0.1;
+    requiredAddons[] = {CUP_Weapons_Javelin};
+    version = "1.1";
+    versionStr = "1.1";
+    versionAr[] = {1,1,0};
+    author[] = {"Winter"};
+    authorUrl = "https://github.com/Winter259";
   };
 };
 
@@ -17,7 +16,7 @@ class CfgWeapons {
   class Launcher_Base_F;
   // CUP_Weapons_Javelin
   class CUP_launch_Javelin: Launcher_Base_F {
-	AGM_Backblast_Range = 0; // Javelin is soft launch
+    AGM_Backblast_Range = 0; // Javelin is soft launch
     AGM_enableTopDownAttack = 1;
   };
 };
