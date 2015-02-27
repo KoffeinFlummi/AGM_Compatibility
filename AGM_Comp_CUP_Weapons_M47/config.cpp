ea99399c4ce8a240471bc68a13ae7f7ed4add960
SAM <simon@agius-muscat.net>
2015-02-27 17:08:27 +0100

    Adjusted m47 to realistic levels
    http://www.inetres.com/gp/military/infantry/antiarmor/M47.html

--- AGM_Comp_CUP_Weapons_M47/config.cpp
+++ AGM_Comp_CUP_Weapons_M47/config.cpp
@@ -3,7 +3,7 @@ class CfgPatches {
     units[] = {};
     weapons[] = {};
     requiredVersion = 0.1;
-    requiredAddons[] = {AGM_Comp_CUP_Weapons_Ammunition,CUP_Weapons_Ammunition,CUP_Weapons_M47};
+    requiredAddons[] = {CUP_Weapons_M47};
     version = "1.0";
     versionStr = "1.0";
     versionAr[] = {1,0,0};
@@ -15,10 +15,9 @@ class CfgPatches {
 class CfgWeapons {
   class Launcher_Base_F;
   // CUP_Weapons_M47
-  // Requires adjusting to realistic values
   class CUP_launch_M47: Launcher_Base_F {
     AGM_Backblast_Angle = 45;
-    AGM_Backblast_Range = 100;
+    AGM_Backblast_Range = 30;
     AGM_Backblast_Damage = 0.7;
   };
 };
